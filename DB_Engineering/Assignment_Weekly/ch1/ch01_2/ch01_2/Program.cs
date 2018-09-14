de966ed75bf96e0d4a155546d3e0ed0b50f3e993
hg.llk <1148684516@qq.com>
2018-09-14 23:19:17 +0800

    Add Course
    DB Engineering

---
+++ DB_Engineering/Assignment_Weekly/ch1/ch01_2/ch01_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch01_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入您的姓名：");
+            String user_name = Console.ReadLine();
+            Console.WriteLine("欢迎您，" + user_name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DB_Engineering/Assignment_Weekly/ch1/ch01_3/ch01_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch01_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int number = 0;
+            int fir, sec, thi, fort;
+            for (i = 4; i < 10; i++)
+            {
+                number = i * i;
+                thi = number / 10;
+                fort = number % 10;
+                if (thi == fort)
+                {
+                    Console.WriteLine("The plate number of the cat is 00" + number);
+                }
+            }
+
+            for (i = 32; i < 100; i++)
+            {
+                number = i * i;
+                fir = number / 1000;
+                sec = number % 1000 / 100;
+                thi = number % 100 / 10;
+                fort = number % 10;
+                if (fir == sec && thi == fort)
+                {
+                    Console.WriteLine("The plate number of the cat is " + number);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
