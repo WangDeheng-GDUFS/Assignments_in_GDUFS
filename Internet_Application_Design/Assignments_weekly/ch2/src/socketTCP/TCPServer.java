5f89bc5104674b175fae43808021218c459a8b23
hg.llk <1148684516@qq.com>
2018-09-25 14:05:02 +0800

    ch3
    Chapter 3 is based on ch2

--- Internet_Application_Design/Assignments_weekly/ch2/src/socketTCP/TCPServer.java
+++ Internet_Application_Design/Assignments_weekly/ch2/src/socketTCP/TCPServer.java
@@ -26,18 +26,20 @@ public class TCPServer {
     while (true) {
       Socket socket=null;
       try {
-     socket = serverSocket.accept(); 
-    //阻塞语句，监听并等待客户发起连接，有连接请求就生成一个套接字。
-     System.out.println("New income: "+socket.getInetAddress());
+      socket = serverSocket.accept(); 
+      //阻塞语句，监听并等待客户发起连接，有连接请求就生成一个套接字。
+      System.out.println("New income: "+socket.getInetAddress());
         //本地服务器观测台显示请求的用户信息。
         BufferedReader br =getReader(socket);//定义字符串输入流。
         PrintWriter pw = putWriter(socket);//定义字符串输出流。
 
         String msg;
         while ((msg = br.readLine())!= null) //阻塞语句，从输入流中读入一行字符串。
-         {
-            pw.println("来自服务器:"+msg);//向输出流中输出一行字符串。
-            //pw.println("来自服务器2:"+msg);
+        {
+            pw.println("来自服务器:" + msg);//向输出流中输出一行字符串。
+            pw.println("来自服务器2:" + msg);
+            pw.println("来自服务器:" + msg);//向输出流中输出一行字符串。
+            pw.println("来自服务器2:" + msg);
             if (msg.equals("bye")) //如果客户发送的消息为“bye”，就结束通信
             break;
         }
